cb076a07d295590c871704578ff67707451c20bc
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2022-04-21 16:00:29 -0700

    manas main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene : [mannyDebug]
-  }
+    scene : [Play, mannyDebug]
+}
+
+// declare keys
+let keyLEFT, keyRIGHT;
 
 // set UI sizes
 let game = new Phaser.Game(config);
