b09d4879c225ca9f939a380b032aff331c0241ac
MannyFluss <manny.fluss@gmail.com>
2022-04-23 14:14:17 -0700

    basic phone made

--- src/mannyDebug.js
+++ src/mannyDebug.js
@@ -11,12 +11,40 @@ class mannyDebug extends Phaser.Scene
         
     }
 
+    updateTextMessage(container, text='default text', playerBool)
+    {
+        var newMessage = new Message(this,0,0,playerBool,text);
+        for (var i=1;i<container.list.length;i++)
+        {
+        //     if (container.list[i].GameObjects == Message)
+        //     {
+        //         console.log("dhasdhasd");
+        //     }
+            
+            container.list[i].y -= container.list[i].height + 10;
+        }
+        container.add([newMessage]);
+
+    }
+
     create()
     {
         
-        this.thePhone = new Phone(this, 300, 300 ,'temp');
-        this.containerTest = new PhoneContainer(this, 100, 100);
-        //this.dbgMsg = new Message(this, 200,200, true, "sample text");
+        var thePhone = new Phone(this, 0, 0 ,'temp');
+
+        var testMessage = new Message(this,0 ,0,true,"test");
+        //container
+        this.containerTest = this.add.container(400, 300);
+        //this.containerTest.x = 0;
+        this.containerTest.add([thePhone,testMessage]);
+        this.containerTest.x = 40
+        console.log(this.containerTest.list);
+        this.updateTextMessage(this.containerTest, "sample", true);
+        this.updateTextMessage(this.containerTest, "sample", true);
+
+
+
+        //this.dbgMsg = new Message(this, 200,200, true, sample text");
     }
 
 }
